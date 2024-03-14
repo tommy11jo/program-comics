4536c86fd78d8938a2aaf5ccaf5c4651bdb68b17
tom11my <tommy11jo@gmail.com>
2024-03-14 12:12:46 -0700

    improve code editor styles

--- react-src/components/CodeEditor.tsx
+++ react-src/components/CodeEditor.tsx
@@ -8,15 +8,25 @@ const CodeEditor = ({ code, lang }: CodeEditorProps) => {
   const customStyle = {
     ...vscDarkPlus,
     'pre[class*="language-"]': {
-      fontFamily: '"Roboto Mono", monospace',
+      background: "#000",
+      fontFamily: 'Cursive, "Roboto Mono", monospace',
+      lineHeight: "normal",
+      fontSize: "16px",
+      color: "white", // workaround to override defaults
+      overflow: "auto",
     },
+    // combining these class does not work, why?
     'code[class*="language-"]': {
-      fontFamily: '"Roboto Mono", monospace',
+      fontSize: "16px",
+      color: "white",
+      overflow: "auto",
     },
   }
+
+  // Note: Manually set bg to black in /node_modules/react-syntax-highlighter/dist/cjs/styles/prism/vsc-dark-plus.js
   return (
-    <div className="p-2 outline outline-[#333] rounded-lg">
-      <SyntaxHighlighter language="python" style={customStyle}>
+    <div className="p-1 md:p-8 outline outline-[#333] rounded-lg">
+      <SyntaxHighlighter language={lang} style={customStyle}>
         {code}
       </SyntaxHighlighter>
     </div>
